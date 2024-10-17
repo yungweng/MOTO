8171c0be6ebee3a977c9235caf36fcaecab3f833
Yannick Wenger <y.wenger@gmx.de>
2024-10-17 13:46:02 +0200

    osDetection Anpassung. Erkennt nun RasPi und wird wie ein Tablet behandelt.

--- moto/main_app/middleware/osDetection.py
+++ moto/main_app/middleware/osDetection.py
@@ -10,25 +10,29 @@ class OsDetectionMiddleware:
 
     def __call__(self, request):
         user_agent = request.META.get('HTTP_USER_AGENT', '')
+        print(user_agent)
         path = request.path
         name_path = ''
+        scanner_os = ['Android', 'Raspbian', 'Debian', 'CrOS', 'python-requests'] # List of OS that are used for the scanner
         try:
             match = resolve(path)
             name_path = match.url_name
         except:
             pass
-        if not 'Android' in user_agent:    #IOS for debugging and testing    
-            if not (path in allowed_urls_web or name_path in allowed_urls_web):
-                return HttpResponseRedirect(reverse(main_url_web)) 
-        elif 'Android' in user_agent:        #IOS for debugging and testing
+
+        if any(os in user_agent for os in scanner_os): # Scanner
             if(path in allowed_urls_android or name_path in allowed_urls_android):
                 if 'device_id' in request.COOKIES:
-                    device_id = request.COOKIES.get('device_id')                
+                    device_id = request.COOKIES.get('device_id')
                     if not(Raum_Belegung.objects.filter(tablet_id=device_id).exists()):
                         if not(path in allowed_urls_android_no_room or name_path in allowed_urls_android_no_room):
                             return HttpResponseRedirect(reverse(main_url_android_no_room))
             else:
-                return HttpResponseRedirect(reverse(main_url_android))    
+                return HttpResponseRedirect(reverse(main_url_android))
+
+        else: # Web
+            if not (path in allowed_urls_web or name_path in allowed_urls_web):
+                return HttpResponseRedirect(reverse(main_url_web))
         response = self.get_response(request)
 
         return response
